9b8ccbc08e1cc8b5a50d302b806a1371e39bccaa
Jakub-Pazio <19jakubpazio99@gmail.com>
2024-06-11 13:17:10 +0200

    You can change color of page xD

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const c_node = document.getElementById("counter");
 const inc_b = document.getElementById("inc");
 const dec_b = document.getElementById("dec");
+const bg_text = document.getElementById("color_input");
+const bg_button = document.getElementById("set_bc");
 
 let counter_val = 0;
 
@@ -12,10 +14,14 @@ const render_counter = () => {
 inc_b.addEventListener('click', () => {
     counter_val++;
     render_counter();
-})
+});
 
 dec_b.addEventListener('click', () => {
     counter_val--;
     render_counter();
-})
+});
 
+bg_button.addEventListener('click', () => {
+    const user_color = bg_text.value;
+    document.body.style.background = user_color;
+});
